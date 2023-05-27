7b03646eb4fd2fc1badbf1a2eedebe7b54dc82d8
leonelfm <leonelfigueredomilanes181@gmail.com>
2023-05-27 02:16:07 +0200

    layout_activity_add_customer is finished but not reviewed

---
+++ app/src/main/java/com/qnecesitas/elretenbombas/Activity_AddCustomer.kt
@@ -0,0 +1,11 @@
+package com.qnecesitas.elretenbombas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_AddCustomer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_customer)
+    }
+}
